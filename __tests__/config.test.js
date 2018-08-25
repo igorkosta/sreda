91bc5fc9c25d64c47781d5f82954862ba1c4c8f4
Igor Kostyuchenok <igor.kostyuchenok@gmail.com>
2018-08-25 23:08:58 +0200

    Properly mocking the AWS.SSM's promisified version

--- __tests__/config.test.js
+++ __tests__/config.test.js
@@ -1,11 +1,39 @@
 /* eslint-env jest */
 'use strict'
 
-// const AWS = require('aws-sdk')
-const AWS = require('aws-sdk-mock')
+const { load } = require('../lib/config')
+const AWS = require('aws-sdk')
+let ssm = new AWS.SSM()
 
-const { load, ssm } = require('../lib/config')
-ssm.getParameters = jest.fn()
+const ssmPromise = {
+  promise: jest.fn().mockImplementation((request) => {
+    return new Promise((resolve, reject) => {
+      const response = {
+        Parameters: [
+          {
+            Name: 'bar',
+            Type: 'String',
+            Value: 'barfoorista',
+            Version: 1,
+            LastModifiedDate: '2018-08-22T13:49:55.717Z',
+            ARN: 'arn:aws:ssm:eu-west-1:243341595825:parameter/bar'
+          },
+          {
+            Name: 'foo',
+            Type: 'String',
+            Value: 'isitbecauseIamblack?',
+            Version: 1,
+            LastModifiedDate: '2018-08-22T13:49:41.486Z',
+            ARN: 'arn:aws:ssm:eu-west-1:243341595825:parameter/foo'
+          }
+        ],
+        InvalidParameters: []
+      }
+      resolve(response)
+    })
+  })
+}
+ssm = { getParameters: () => { return ssmPromise } }
 
 describe('mock AWS.SSM()', () => {
   beforeAll(async () => {
@@ -13,33 +41,7 @@ describe('mock AWS.SSM()', () => {
   })
 
   it('getParameters', async () => {
-    const response = {
-      Parameters: [
-        {
-          Name: 'bar',
-          Type: 'String',
-          Value: 'barfoorista',
-          Version: 1,
-          LastModifiedDate: '2018-08-22T13:49:55.717Z',
-          ARN: 'arn:aws:ssm:eu-west-1:243341595825:parameter/bar'
-        },
-        {
-          Name: 'foo',
-          Type: 'String',
-          Value: 'isitbecauseIamblack?',
-          Version: 1,
-          LastModifiedDate: '2018-08-22T13:49:41.486Z',
-          ARN: 'arn:aws:ssm:eu-west-1:243341595825:parameter/foo'
-        }
-      ],
-      InvalidParameters: []
-    }
-
-    AWS.mock('SSM', 'getParameters', function (params, callback) {
-      callback(null, response)
-    })
-
-    const configResponse = load(['foo', 'bar'])
+    const configResponse = load(ssm, ['foo', 'bar'])
     // const params = await ssm.getParameters()
     const r = await configResponse.keys.foo
     console.log(r)
